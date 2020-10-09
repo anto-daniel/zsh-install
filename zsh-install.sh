53c25676d5d31eae19b711b3b2ac7d5126580b04
Anto Daniel <anto.daniel@gmail.com>
2020-10-10 01:19:54 +0530

    adding executable permission to zsh-install script

--- zsh-install.sh
+++ zsh-install.sh
@@ -4,3 +4,7 @@ echo "Gonna Install oh my zsh !!!"
 sudo apt-get update && sudo apt-get -y install zsh wget
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i "s/robbyrussell/mrtazz/g" ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i "/plugins/s/)/ zsh-autosuggestions)/" ~/.zshrc
+source ~/.zshrc
