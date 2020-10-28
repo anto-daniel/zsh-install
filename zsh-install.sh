8d52aa49a09ade0c56a6d15af9e12ed2b83bd919
ci-test <anto.daniel@gmail.com>
2020-10-28 18:48:26 +0530

    test

--- zsh-install.sh
+++ zsh-install.sh
@@ -6,5 +6,6 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 sed -i "s/robbyrussell/mrtazz/g" ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "adding zsh-autosuggestion"
 sed -i "/plugins/s/)/ zsh-autosuggestions)/" ~/.zshrc
 source ~/.zshrc
