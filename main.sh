d45dc6d7a27c791351e6682848db91c8fed9dada
anto-daniel <anto.daniel@gmail.com>
2022-06-13 21:42:44 +0530

    adding main script

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+./zsh-install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "adding zsh-autosuggestion"
+sed -i "/plugins/s/)/ zsh-autosuggestions)/" ~/.zshrc
+source ~/.zshrc
