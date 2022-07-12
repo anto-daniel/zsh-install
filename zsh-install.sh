aade35a70a9dee2a8497ae4b31011b05017ce083
Anto Daniel <anto.daniel@gmail.com>
2022-07-12 14:50:10 +0530

    Precheck on zshrc

--- zsh-install.sh
+++ zsh-install.sh
@@ -2,5 +2,8 @@
 
 echo "Gonna Install oh my zsh !!!"
 sudo apt-get update && sudo apt-get -y install zsh wget direnv
+if [ ! -f ~/.zshrc ]; then
+	echo "# creating runtime config" > .zshrc
+fi
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i "s/robbyrussell/mrtazz/g" ~/.zshrc
