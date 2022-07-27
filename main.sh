62d5b5abff61f50670aa62cd8a3064498ba47344
Anto Daniel <anto.daniel@gmail.com>
2022-07-27 14:20:03 +0530

    test

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ set -x
 ./zsh-install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "adding zsh-autosuggestion"
-sed -i "/plugins/s/)/ direnv zsh-autosuggestions)/" ~/.zshrc
+sed -i "/plugins/s/)/ zsh-autosuggestions)/" ~/.zshrc
 source ~/.zshrc
