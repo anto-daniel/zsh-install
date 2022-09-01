5366ffd68addb84d6af8ba15b2b429484bc21bb8
Anto Daniel <anto.daniel@gmail.com>
2022-09-01 16:26:39 +0530

    correcting path

--- zsh-install.sh
+++ zsh-install.sh
@@ -3,7 +3,7 @@
 echo "Gonna Install oh my zsh !!!"
 sudo apt-get update && sudo apt-get -y install zsh wget direnv
 if [ ! -f ~/.zshrc ]; then
-	echo "# creating runtime config" > .zshrc
+	echo "# creating runtime config" > $HOME/.zshrc
 fi
 sudo apt-get update && sudo apt-get -y install zsh wget
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
