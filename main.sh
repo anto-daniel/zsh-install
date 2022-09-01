7d990a76b19085128b758285a557df5b86d3b9fb
Anto Daniel <anto.daniel@gmail.com>
2022-09-01 16:23:59 +0530

    correction

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@
 set -x
 
 ./zsh-install.sh
+if [ $? -ne 0 ]; then echo zsh is not installed properly;exit 1;fi
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "adding zsh-autosuggestion"
 sed -i "/plugins/s/)/ zsh-autosuggestions)/" ~/.zshrc
--- zsh-install.sh
+++ zsh-install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 echo "Gonna Install oh my zsh !!!"
-<<<<<<< HEAD
-<<<<<<< HEAD
 sudo apt-get update && sudo apt-get -y install zsh wget direnv
 if [ ! -f ~/.zshrc ]; then
 	echo "# creating runtime config" > .zshrc
 fi
-=======
 sudo apt-get update && sudo apt-get -y install zsh wget
->>>>>>> parent of ccd8277... adding direnv plugin
-=======
-sudo apt-get update && sudo apt-get -y install zsh wget
->>>>>>> parent of ccd8277... adding direnv plugin
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i "s/robbyrussell/mrtazz/g" ~/.zshrc
